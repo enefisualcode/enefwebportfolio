be83059f36c344b7cf8d5065c3d4e08d97b51020
enefisualcode <nandanabil021@gmail.com>
2023-06-28 11:24:44 +0700

    tambah 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ modalCloses.forEach((modalClose) => {
 
 // ========== SWIPER ===========
 
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
   cssMode: true,
   loop: true,
 
@@ -106,5 +106,24 @@ let swiper = new Swiper('.portfolio__container', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+});
+
+// ========== TESTIMONIAL ===========
+
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidePerview: 2,
+    },
   },
 });
